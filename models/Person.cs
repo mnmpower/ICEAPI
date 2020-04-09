1d01d01228260f311d3f2b2a5d81f146842af916
Maarten Michiels <mnmmaarten@hotmail.com>
2020-04-09 14:55:33 +0200

    added models

---
+++ models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ICE_API.models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public ICollection<Project> Projects { get; set; }
+    }
+}
---
+++ models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ICE_API.models
+{
+    public class Project
+    {
+        [Key]
+        public int ProjectID { get; set; }
+        public string Title { get; set; }
+        public string IFrame { get; set; }
+        public string Description { get; set; }
+        public int PersonID { get; set; }
+
+        [ForeignKey("PersonID")]
+        public virtual Person Person { get; set; }
+
+    }
+}
